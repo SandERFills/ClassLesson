164fd32631937c28daad16ccb7ca1ef209d2f707
SandERFills <salagaev98@mail.ru>
2021-04-29 21:57:07 +0300

    First try to learn OOP

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace TestClass
+{
+   public class Human {
+        
+        private string Name;
+        public string NameProp
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+        private string SecondName;
+        public string SecondNameProp
+        {
+            get { return SecondName; }
+            set { SecondName = value; }
+        }
+        
+            }
+            public class Student : Human {
+              public  Student(string name,string secondname){
+                    NameProp=name;
+                    SecondNameProp=secondname;
+                }
+            }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human Student =new Student("Vlad","Babich");
+           
+            System.Console.WriteLine(Student.NameProp+" "+Student.SecondNameProp);
+            
+        }
+    }
+}
